6a30eaf8af991465a865d62ec2f8f194c68d386a
Nicolás Poza <npoza.vrgroup@bicevida.cl>
2024-08-14 12:26:42 -0400

    feat: first commit springDemo

--- src/main/java/com/example/controllers/IndexController.java
+++ src/main/java/com/example/controllers/IndexController.java
@@ -1,5 +1,13 @@
-package com.example.demo.controllers;
+package com.example.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class IndexController {
 
+    @GetMapping(value = "/")
+    public String Index() {
+        return "index";
+    }
 }
